00a51cb0c65ba888a6f30d36e18d4e738a5f756b
yani92 <yani.outtagarts@gmail.com>
2025-01-09 11:46:22 +0100

    menu.js

---
+++ menu.js
@@ -0,0 +1,9 @@
+let menuMobile = document.querySelector('#menuMobile');
+let nav = document.querySelector('nav');
+
+menuMobile.addEventListener('click', function()
+{
+    nav.classList.toggle('helloNav');
+});
+
+console.log(menuMobile);
