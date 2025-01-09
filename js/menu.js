c3b4fd9b6760e84757ca8d8542c79f5e66dea351
yani92 <yani.outtagarts@gmail.com>
2025-01-09 11:59:55 +0100

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,8 @@
-//deux objets :
 let menuMobile = document.querySelector('#menuMobile');
 let nav = document.querySelector('nav');
 
-// quand je clique sur le menuMobile
 menuMobile.addEventListener('click', function()
 {
-    //je dois voir la nav ou la cacher...
-    // bascule entre le display block / none
-    // bascule nav / .helloNav
     nav.classList.toggle('helloNav');
 });
 
